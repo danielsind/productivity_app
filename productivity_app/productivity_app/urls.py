326463a70917a3f26b3d5ee97342313af76856de
danielsind <danielsindet@gmail.com>
2023-07-26 09:32:56 +0300

    included the path for task_list

--- productivity_app/productivity_app/urls.py
+++ productivity_app/productivity_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('tasks/', include('task.urls'))
+    path('tasks/', include('task.urls')),
+    path('tasks_list/', include('tasks_list.urls'))
 ]
