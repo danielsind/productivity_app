39e122c8a40ca7816aea906cf0e436d9f9044ae8
danielsind <danielsindet@gmail.com>
2023-07-26 09:07:00 +0300

    Changed attribute description to null

--- productivity_app/task/models.py
+++ productivity_app/task/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000, null=True)
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
 
