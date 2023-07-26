c502144b89855b79cd022149cd83b18de896cd7f
danielsind <danielsindet@gmail.com>
2023-07-26 10:13:05 +0300

    Created a model List, added get_user

--- productivity_app/tasks_list/models.py
+++ productivity_app/tasks_list/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from utils.models import Timestamps
 
 # Create your models here.
 class List(Timestamps, models.Model):
-    user_id = models.ForeignKey(User, )
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     description = models.TextField(null=True)
