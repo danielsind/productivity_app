447872bb020ee84c8818d1504929ff29c11920db
danielsind <danielsindet@gmail.com>
2023-07-26 09:34:03 +0300

    Created a models called List

--- productivity_app/tasks_list/models.py
+++ productivity_app/tasks_list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from utils.models import Timestamps
 
 # Create your models here.
+class List(Timestamps, models.Model):
+    user_id = models.ForeignKey(User, )
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True)
