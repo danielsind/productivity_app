932ac3c7a1258d97148aec1ef2ec0044ffde2c8e
danielsind <danielsindet@gmail.com>
2023-07-27 09:10:32 +0300

    Created a path for users

--- productivity_app/productivity_app/urls.py
+++ productivity_app/productivity_app/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('tasks/', include('task.urls')),
-    path('tasks_list/', include('tasks_list.urls'))
+    path('tasks_list/', include('tasks_list.urls')),
+    path('users/', include('users.urls'))
 ]
--- productivity_app/tasks_list/views.py
+++ productivity_app/tasks_list/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from .models import List
+
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = List
+        fields = ['name', 'description', 'date_created', 'date_updated']
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+
 
 # Create your views here.
