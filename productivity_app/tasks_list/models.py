0af4ca4d6daaa380551253098a140049de84c9f4
danielsind <danielsindet@gmail.com>
2023-07-27 09:13:19 +0300

    Create a model ListItem and inherited attribute Timestamp

--- productivity_app/tasks_list/models.py
+++ productivity_app/tasks_list/models.py
@@ -7,3 +7,10 @@ class List(Timestamps, models.Model):
     user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     description = models.TextField(null=True)
+
+class ListItem(Timestamps, models.Model):
+    parent_list = models.ForeignKey(List, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.text
