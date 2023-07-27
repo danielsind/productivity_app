f82733ba788b14e9c9b23df70e0334bce61f38d6
danielsind <danielsindet@gmail.com>
2023-07-27 09:23:17 +0300

    White space on views.py

--- productivity_app/tasks_list/views.py
+++ productivity_app/tasks_list/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import serializers, viewsets
 from .models import List
+from .models import ListItem
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField()
     class Meta:
         model = List
         fields = ['name', 'description', 'date_created', 'date_updated']
@@ -10,6 +12,17 @@ class ListSerializer(serializers.HyperlinkedModelSerializer):
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+
+
+class ListItemSerializer(serializers.HyperlinkedModelSerializer):
+    parent_list = serializers.PrimaryKeyRelatedField()
+    class Meta:
+        model = ListItem
+        fields = ['name', 'description', 'date_created', 'date_updated']
+
+class ListItemViewSet(viewsets.ModelViewSet):
+    queryset = ListItem.objects.all()
+    serializer_class = ListItemSerializer
 
 
 
