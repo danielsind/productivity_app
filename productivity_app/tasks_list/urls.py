818a4f81c30f2bd4a2b40cf89d367d21fccbfb18
danielsind <danielsindet@gmail.com>
2023-07-28 16:44:02 +0300

    Defined routers

--- productivity_app/tasks_list/urls.py
+++ productivity_app/tasks_list/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ListViewSet
+from rest_framework_nested import routers
+from .views import ListViewSet, ListItemViewSet
 
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'', ListViewSet)
+# router.register(r'listitem', ListItemViewSet)
+lists_router = routers.NestedSimpleRouter(router, r'', lookup = 'lists')
+lists_router.register(r'list-item', ListItemViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path(r'', include(router.urls)),
+    path(r'', include(lists_router.urls))
 ]
