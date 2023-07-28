512c241cd9d93f0b13e4c7622327c42942a51feb
danielsind <danielsindet@gmail.com>
2023-07-28 16:44:58 +0300

    Updated serializers

--- productivity_app/tasks_list/views.py
+++ productivity_app/tasks_list/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rest_framework import serializers, viewsets
 from .models import List
 from .models import ListItem
+from django.contrib.auth import get_user_model
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.PrimaryKeyRelatedField()
+    user = serializers.PrimaryKeyRelatedField(queryset = get_user_model().objects.all())
     class Meta:
         model = List
-        fields = ['name', 'description', 'date_created', 'date_updated']
+        fields = ['user','name', 'description', 'date_created', 'date_updated']
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
@@ -15,10 +16,10 @@ class ListViewSet(viewsets.ModelViewSet):
 
 
 class ListItemSerializer(serializers.HyperlinkedModelSerializer):
-    parent_list = serializers.PrimaryKeyRelatedField()
+    parent_list = serializers.PrimaryKeyRelatedField(queryset = List.objects.all())
     class Meta:
         model = ListItem
-        fields = ['name', 'description', 'date_created', 'date_updated']
+        fields = ['name', 'description', 'date_created', 'date_updated','parent_list']
 
 class ListItemViewSet(viewsets.ModelViewSet):
     queryset = ListItem.objects.all()
